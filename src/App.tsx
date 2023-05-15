b3824480cfb8a4f8b5e9edaf0ab220d04f322b6b
AGindullin <GA>
2023-05-15 21:50:56 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import { FormikForm } from './components/Formik/Formik';
-import { QuestionForm } from './components/QuestionForm/QuestionForm';
-import { Button, Stack, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { FormsPage } from './pages/FormsPage';
 
 function App() {
-    const { t, i18n } = useTranslation();
+    const { i18n } = useTranslation();
 
     const changeLanguage = (lang: string) => {
         i18n.changeLanguage(lang);
@@ -14,20 +13,8 @@ function App() {
         <>
             <Button onClick={() => changeLanguage('en')}>EN</Button>
             <Button onClick={() => changeLanguage('ru')}>RU</Button>
-            <div className="App">
-                <header className="App-header">
-                    <Typography textOverflow="ellipsis" variant="h2">
-                        {t('workingWithForms')}
-                    </Typography>
-                    <Stack
-                        direction="row"
-                        justifyContent="space-evenly"
-                        spacing={2}
-                    >
-                        <FormikForm />
-                        <QuestionForm />
-                    </Stack>
-                </header>
+            <div className="Wrap">
+                <FormsPage />
             </div>
         </>
     );
--- src/pages/FormsPage.tsx
+++ src/pages/FormsPage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import { FormikForm } from '../components/Formik/Formik';
+import { QuestionForm } from '../components/QuestionForm/QuestionForm';
+import { useTranslation } from 'react-i18next';
 
 export const FormsPage = () => {
-    return <div>1</div>;
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <Typography textOverflow="ellipsis" variant="h2">
+                {t('workingWithForms')}
+            </Typography>
+            <Stack direction="row" justifyContent="space-evenly" spacing={2}>
+                <FormikForm />
+                <QuestionForm />
+            </Stack>
+        </>
+    );
 };
